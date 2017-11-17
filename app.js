679cebf7bd053b7d4417454976f2602981310150
Anders <Anrskp@gmail.com>
2017-11-17 13:25:40 +0100

    test

---
+++ app.js
@@ -0,0 +1,51 @@
+// Imports
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database')
+const users = require('./routes/users');
+
+const app = express();
+
+// Mongoose promise libary
+mongoose.Promise = require('bluebird');
+
+// Connect To Database
+mongoose.connect(config.database, {
+  useMongoClient: true
+});
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database ' + config.database)
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error : ' + err);
+});
+
+// CORS Middleware
+app.use(cors());
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', users);
+
+// Start Server
+const port = 3000;
+app.listen(port, () => {
+   console.log('Server startet on port ' + port);
+});
---
+++ models/posts.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const config = require('../config/database');
+
+// Post Schema
+const postSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  post: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  lastEdit : {
+    type: Date,
+    required: false
+  }
+});
+
+const Post = module.exports = mongoose.model('Post', postSchema);
+
+// Methods
+// todo : decrypt post
+module.exports.getPostByUsername = (username, callback) => {
+  const query = {username: username};
+  Post.findOne(query, callback);
+}
+
+// todo : decrypt post
+module.exports.getPostById = (id, callback) => {
+  Post.findById(id, callback);
+}
+
+// todo : encrypt post
+module.exports.addPost = function(newPost, callback) {
+    newPost.save(callback);
+    });
+  });
+}
