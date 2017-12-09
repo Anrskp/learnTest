76a60cee6849f87e93cca6ace9f4a00a5f94cdf1
Anders <Anrskp@gmail.com>
2017-12-09 18:25:21 +0100

    deploy test

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(passport.session());
 require('./config/passport')(passport);
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, './angular-src/dist')));
+app.use(express.static(path.join(__dirname, 'angular-src/dist')));
 
 app.use('/users', users);
 app.use('/posts', posts);
@@ -64,7 +64,7 @@ res.send('Invalid Endpoint');
 });
 
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, './angular-src/dist/index.html'));
+res.sendFile(path.join(__dirname, 'angular-src/dist/index.html'));
 });
 
 // IO Connection
