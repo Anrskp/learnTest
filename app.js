fb86830f62850eddc30d5d7ac5c473845edacf03
Anders <Anrskp@gmail.com>
2017-12-09 18:23:45 +0100

    deploy test

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ mongoose.Promise = require('bluebird');
 
 // Connect To Database
 mongoose.connect(config.database, {
-  useMongoClient: true
+useMongoClient: true
 });
 
 // On Connection
 mongoose.connection.on('connected', () => {
-  //console.log('Connected to database ' + config.database)
-  console.log('Connected to database ' + config.database)
+//console.log('Connected to database ' + config.database)
+console.log('Connected to database ' + config.database)
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-  console.log('Database error : ' + err);
+console.log('Database error : ' + err);
 });
 
 // CORS Middleware
@@ -53,57 +53,57 @@ app.use('/users', users);
 app.use('/posts', posts);
 
 // Start Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log('Server startet on port ' + port);
+console.log('Server startet on port ' + port);
 });
 
 // Index Route
 app.get('/', (req, res) => {
-  res.send('Invalid Endpoint');
+res.send('Invalid Endpoint');
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './angular-src/dist/index.html'));
+res.sendFile(path.join(__dirname, './angular-src/dist/index.html'));
 });
 
 // IO Connection
 io.use(function(socket, next){
-  // Authentication
-  if (socket.handshake.query && socket.handshake.query.token){
-    // todo : token has "JWT " infront of it - dosent work withit here, why elsewhere?
-    jwt.verify(socket.handshake.query.token.substring(4), 'My Secret', function(err, decoded) {
-      if(err) return next(new Error('Authentication error'));
-      socket.decoded = decoded;
-      next();
-    });
-  }
-  next(new Error('Authentication error'));
+// Authentication
+if (socket.handshake.query && socket.handshake.query.token){
+  // todo : token has "JWT " infront of it - dosent work withit here, why elsewhere?
+  jwt.verify(socket.handshake.query.token.substring(4), 'My Secret', function(err, decoded) {
+    if(err) return next(new Error('Authentication error'));
+    socket.decoded = decoded;
+    next();
+  });
+}
+next(new Error('Authentication error'));
 })
 
 // Connection now authenticated to receive further events
 .on('connection', function(socket) {
-  console.log('new socket connection established')
+console.log('new socket connection established')
 
-  // On reciving a new message
-  socket.on('send message', function (data) {
-    let encryptedPost = CryptoJS.AES.encrypt(data.post, 'My secret');
+// On reciving a new message
+socket.on('send message', function (data) {
+  let encryptedPost = CryptoJS.AES.encrypt(data.post, 'My secret');
 
-    let newPost = new Post ({
-      username: data.username,
-      post: data.post,
-      date: data.date,
-    });
-
-    // Broadcast post to clients
-    io.emit('receive message', newPost);
-    // Encrypt post
-    newPost.post = encryptedPost
-    // Save post in db.
-    Post.addPost(newPost, (err, post) => {
-      if(err) throw err;
-    });
+  let newPost = new Post ({
+    username: data.username,
+    post: data.post,
+    date: data.date,
   });
+
+  // Broadcast post to clients
+  io.emit('receive message', newPost);
+  // Encrypt post
+  newPost.post = encryptedPost
+  // Save post in db.
+  Post.addPost(newPost, (err, post) => {
+    if(err) throw err;
+  });
+});
 });
 
 module.exports = app; // export for testing
